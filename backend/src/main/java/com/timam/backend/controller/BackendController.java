0532f0144045e907f22b72457ddd4b011b4166ae
Al-Amin Talukdar <md.talukdar@bkash.com>
2020-10-09 03:39:44 +0600

    fixed

--- backend/src/main/java/com/timam/backend/controller/BackendController.java
+++ backend/src/main/java/com/timam/backend/controller/BackendController.java
@@ -1,12 +1,19 @@
-package com.timam.backend.response;
+package com.timam.backend.controller;
 
+import com.timam.backend.response.Result;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class Result {
-    private String message;
+@RestController
+public class BackendController {
+    @Value("${app.profile}")
     private String profile;
+
+    @GetMapping(value = "/backend")
+    public Result getResult(){
+        Result result = Result.builder().message("Hello").profile(profile).build();
+        return result;
+    }
+
 }
--- backend/src/main/java/com/timam/backend/response/Result.java
+++ backend/src/main/java/com/timam/backend/response/Result.java
@@ -1,19 +1,12 @@
-package com.timam.backend.controller;
+package com.timam.backend.response;
 
-import com.timam.backend.response.Result;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BackendController {
-    @Value("${app.profile}")
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Result {
+    private String message;
     private String profile;
-
-    @GetMapping(value = "/backend")
-    public Result getResult(){
-        Result result = Result.builder().message("Hello").profile(profile).build();
-        return result;
-    }
-
 }
