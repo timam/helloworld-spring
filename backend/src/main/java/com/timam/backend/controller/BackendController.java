9372d0f78542b17f49d7bdc3c0f71e3f9080c411
Ruhshan <ruhshan.ahmed@gmail.com>
2020-11-10 18:58:38 +0600

    Add logback configurations for frontend and backend

--- backend/src/main/java/com/timam/backend/controller/BackendController.java
+++ backend/src/main/java/com/timam/backend/controller/BackendController.java
@@ -1,11 +1,13 @@
 package com.timam.backend.controller;
 
 import com.timam.backend.response.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class BackendController {
     @Value("${app.profile}")
     private String profile;
@@ -13,6 +15,7 @@ public class BackendController {
     @GetMapping(value = "/")
     public Result getResult(){
         Result result = Result.builder().message("Hello").profile(profile).build();
+        log.info("Writing log from backend");
         return result;
     }
 
--- frontend/src/main/java/com/timam/frontend/FrontendController.java
+++ frontend/src/main/java/com/timam/frontend/FrontendController.java
@@ -2,11 +2,16 @@ package com.timam.frontend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @RestController
 public class FrontendController {
     @GetMapping("/")
     public String sayHello(){
+
+        log.info("Writing from frontend");
         return "Hello from frontend";
     }
 }
